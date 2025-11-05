adda39bcc222ebc9d2f33f2b4c489f8ba21b4497
TopAnter <topsumikael.maki@gmail.com>
2025-11-05 22:15:51 +0200

    Update App.jsx

--- Osa-1/1,1/src/App.jsx
+++ Osa-1/1,1/src/App.jsx
@@ -1,3 +1,48 @@
+const Header = (props) => {
+  return (
+      <h1>{props.course}</h1>
+      
+  )
+}
+const Part1 = (props) => {
+  return (
+    <p>
+      {props.part1} {props.exercises1}
+    </p>
+  )
+}
+
+const Part2 = (props) => {
+  return (
+    <p>
+      {props.part2} {props.exercises2}
+    </p>
+  )
+}
+
+const Part3 = (props) => {
+  return (
+    <p>
+      {props.part3} {props.exercises3}
+    </p>
+  )
+}
+const Content = (props) => {
+    return (
+      <div>
+        <Part1 part1={props.part1} exercises1={props.exercises1} />
+        <Part2 part2={props.part2} exercises2={props.exercises2} />
+        <Part3 part3={props.part3} exercises3={props.exercises3} />
+        
+      </div>
+    )
+}
+
+const Total = (props) => {
+  return(
+    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  )
+}
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -9,17 +54,20 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course}/>
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
     </div>
   )
 }
