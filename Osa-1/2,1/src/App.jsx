4f519ea44cdffc5de38fc610bc26d3aa459b7121
TopAnter <topsumikael.maki@gmail.com>
2025-11-12 22:16:59 +0200

    k

---
+++ Osa-1/2,1/src/App.jsx
@@ -0,0 +1,54 @@
+const Content = (props) => {
+  return (
+    <div>
+      {props.osat.map(osa => (
+        <p key={osa.id}>
+          {osa.name} {osa.exercises}
+        </p>
+      ))}
+    </div>
+  )
+}
+const Course = ({ course }) => {
+  
+  return (
+    <div>
+      <h1>{course.name}</h1>
+      <Content
+        osat = {course.parts}
+        
+      />
+    </div>
+  )
+}
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
