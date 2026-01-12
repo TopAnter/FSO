4267b2489986a85a012b9da5066647f7ebd14334
TopAnter <topsumikael.maki@gmail.com>
2026-01-12 10:34:29 +0200

    .

--- Osa-3/3,1/index.js
+++ Osa-3/3,1/index.js
@@ -1,27 +1,103 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
-let notes = [
+let persons = [
   {
-    id: "1",
-    content: "HTML is easy",
-    important: true
+    id: '1',
+    name: "Arto Hellas",
+    number: "040-123456"
   },
   {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
+    id: '2',
+    name: "Ada Lovelace",
+    number: "040-223456"
   },
   {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
+    id: '3',
+    name: "Den Abramov",
+    number: "040-333456"
+  },
+  {
+    id: '4',
+    name: "Mary Poppendieck",
+    number: "040-444456"
+  },
 ]
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'application/json' })
-  response.end(JSON.stringify(notes))
+
+app.use(express.json())
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+//kaikki henkilöt
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+//yksittäinen henkilö
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find((person) => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+//luo id
+const generateId = () => {
+  const id = Math.floor(Math.random() * 100000) + 1
+  return String(id)
+}
+
+//lisää henkilö
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  //tarkista onko nimi ja numero
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'name or number missing',
+    })
+  }
+
+  //tarkista onko nimi uniikki
+  if (persons.find((person) => person.name === body.name)) {
+    return response.status(400).json({
+      error: 'name must be unique',
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+//poista henkilö
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter((person) => person.id !== id)
+
+  response.status(204).end()
+})
+
+//info page
+app.get('/info', (request, response) => {
+  const listLength = persons.length
+  const time = new Date().toString()
+  response.send(`Phonebook has info for ${listLength} people <br><br> ${time}`)
 })
 
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
