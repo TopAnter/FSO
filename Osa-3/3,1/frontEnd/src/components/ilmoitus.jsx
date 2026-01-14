3c4f8a41d77e592096a45c4fed5471b9091cb4cf
TopAnter <topsumikael.maki@gmail.com>
2026-01-14 16:14:05 +0200

    kons

---
+++ Osa-3/3,1/frontEnd/src/components/ilmoitus.jsx
@@ -0,0 +1,28 @@
+const badNotification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className="error">
+      {message}
+    </div>
+  )
+}
+
+const goodNotification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className="success">
+      {message}
+    </div>
+  )
+}
+
+export default {
+    badNotification, 
+    goodNotification
+}
