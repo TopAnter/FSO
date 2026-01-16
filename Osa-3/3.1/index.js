4fafb91cf9c0ff26af2c3f102eff967ceb4284d5
TopAnter <topsumikael.maki@gmail.com>
2026-01-16 14:42:50 +0200

    kyllätoimii

--- Osa-3/3.1/index.js
+++ Osa-3/3.1/index.js
@@ -36,6 +36,7 @@ let persons = [
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
+app.use(express.static('dist'))
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
