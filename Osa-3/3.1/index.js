8cf4c11e022176acd7c22f3b773acb81fa035bcd
TopAnter <topsumikael.maki@gmail.com>
2026-01-16 14:57:45 +0200

    k

--- Osa-3/3.1/index.js
+++ Osa-3/3.1/index.js
@@ -34,9 +34,10 @@ let persons = [
 ]
 
 app.use(express.json())
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-app.use(cors())
 app.use(express.static('dist'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
