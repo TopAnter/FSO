79a6d2ecf040b0eef88e53fd470db52846a95327
TopAnter <topsumikael.maki@gmail.com>
2026-01-22 13:48:44 +0200

    workingTest

---
+++ Osa-4/4.1/controllers/blogPost.js
@@ -0,0 +1,19 @@
+const notesRouter = require('express').Router()
+const Blog = require('../models/blogPost.js')
+
+notesRouter.get('/', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
+})
+
+notesRouter.post('/', (request, response, next) => {
+  const blog = new Blog(request.body)
+
+  blog.save().then((result) => {
+    response.status(201).json(result)
+  })
+})
+
+
+module.exports = notesRouter
