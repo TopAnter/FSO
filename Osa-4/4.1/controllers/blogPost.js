0c3552e9985a02c25f88b9fcf1a6fb1665f00a79
TopAnter <topsumikael.maki@gmail.com>
2026-01-23 15:59:48 +0200

    4.b osa valmiiksi

--- Osa-4/4.1/controllers/blogPost.js
+++ Osa-4/4.1/controllers/blogPost.js
@@ -1,19 +1,40 @@
 const notesRouter = require('express').Router()
 const Blog = require('../models/blogPost.js')
 
-notesRouter.get('/', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
+notesRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
+  
 })
 
-notesRouter.post('/', (request, response, next) => {
+notesRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
+  const result = await blog.save()
+  
+  response.status(201).json(result)
+  
 })
 
+notesRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  response.json(blog)
+})
+
+notesRouter.put('/:id', async (request, response) => {
+  const {title, author, url, likes} = request.body
+
+  const blog = await Blog.findById(request.params.id)
+
+
+  blog.title = title
+  blog.author = author
+  blog.url = url
+  blog.likes = likes
+  const result = await blog.save()
+  
+  
+  response.status(200).json(result)
+  
+})
 
 module.exports = notesRouter
--- Osa-4/4.1/tests/totalLikes.test.js
+++ Osa-4/4.1/tests/totalLikes.test.js
@@ -55,27 +55,40 @@ const blogs = [
 
 
 describe('total likes', () => {
-  const listWithOneBlog = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0
-    }
-  ]
   
-  test('when list has only one blog equals the likes of that', () => {
-    const result = listHelper.totalLikes(listWithOneBlog)
-    assert.strictEqual(result, 5)
-  })
+  if(blogs.length === 0){
+    test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes(blogs)
+    assert.strictEqual(result, blogs[0].likes)
+    })
+  }else{
+    test('of a bigger list is calculated right', () => {
+      const result = listHelper.totalLikes(blogs)
+      assert.strictEqual(result, 36)
+    })
+  }
 })
 
 describe('Most Likes', () => {
 
   test('find the blog with most likes', () => {
-    const result = listHelper.totalLikes(listWithOneBlog)
-    assert.strictEqual(result, 5)
+    const result = listHelper.favoriteBlog(blogs)
+    assert.deepStrictEqual(result, blogs[2])
+  })
+})
+
+describe('Most Blogs', () => {
+
+  test('find the author with most blogs', () => {
+    const result = listHelper.mostBlogs(blogs)
+    assert.deepStrictEqual(result, { author: 'Robert C. Martin', blogs: 3 })
+  })
+})
+
+describe('Most Likes', () => {
+
+  test('find the author with most likes', () => {
+    const result = listHelper.mostLikes(blogs)
+    assert.deepStrictEqual(result, {author: "Edsger W. Dijkstra", likes: 17})
   })
 })
