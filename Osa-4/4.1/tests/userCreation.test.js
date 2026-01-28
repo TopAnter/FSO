13d55049db294866002b25192fedcfb4a634a2e8
TopAnter <topsumikael.maki@gmail.com>
2026-01-28 17:53:10 +0200

    k

---
+++ Osa-4/4.1/tests/userCreation.test.js
@@ -0,0 +1,45 @@
+const bcrypt = require('bcrypt')
+const { test, describe, beforeEach } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const User = require('../models/user')
+const app = require('../app')
+const assert = require('assert')
+
+//npm test -- tests/userCreation.test.js
+
+describe('when there is initially one user at db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+    
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const users = await User.find({})
+    const usersAtStart = users.map(u => u.toJSON())
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    
+    users = await User.find({})
+    const usersAtEnd = users.map(u => u.toJSON())
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    assert(usernames.includes(newUser.username))
+  })
+})
